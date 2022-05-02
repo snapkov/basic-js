82c362026b57e85b660564d4f813cf3903ddc7e0
snapkov <snapkov@gmail.com>
2022-05-03 00:49:12 +0300

    feat: impliment function commonCharacter

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,38 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  const s1Freq = {};
+  const s2Freq = {};
+  let count = 0;
+
+  for (let i = 0; i < s1.length; i++) {
+    if (s1Freq.hasOwnProperty(s1[i])) {
+      s1Freq[s1[i]]++;
+    }
+    else {
+      s1Freq[s1[i]] = 1;
+    }
+  }
+
+  for (let i = 0; i < s2.length; i++) {
+    if (s2Freq.hasOwnProperty(s2[i])) {
+      s2Freq[s2[i]]++;
+    }
+    else {
+      s2Freq[s2[i]] = 1;
+    }
+  }
+
+  for (let key in s1Freq) {
+    if (s2Freq.hasOwnProperty(key)) {
+      count += Math.min(s1Freq[key], s2Freq[key]);
+    }
+  }
+
+  return count;
 }
 
 module.exports = {
