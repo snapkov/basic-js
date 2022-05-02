42cc4197e5501ed962a4e82471609831fb4a0c78
snapkov <snapkov@gmail.com>
2022-05-02 09:23:46 +0300

    feat: impliment function dreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  let dreamTeamShort = [];
+
+  for (let i = 0; i < members.length; i++) {
+    if (typeof(members[i]) === "string") {
+      dreamTeamShort.push(members[i].trim()[0].toUpperCase());
+    }
+  }
+
+  return dreamTeamShort.length > 0 ? dreamTeamShort.sort((a, b) => a.localeCompare(b)).join("") : false;
 }
 
 module.exports = {
