814c025d297fa7fc496ec2e72516e8e891f769c4
snapkov <snapkov@gmail.com>
2022-05-02 15:35:16 +0300

    feat: impliment function hanoiTower

--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
+function getSeason(/*date*/) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (arguments.length === 0) {
+    return "Unable to determine the time of year!";
+  }
+
+  function isInvalidDate(date) {
+    if (!(date instanceof Date)) {
+      // && !isNaN(Date.parse(date))
+      // if (!(Object.prototype.toString.call(date) === "[object Date]")) {
+        throw "Invalid date!";
+      }
+  }
+
+  try {
+    isInvalidDate(date);
+  } catch (e) {
+    console.error(e);
+  }
+
+  let monthBySeason = ["winter", "winter", "spring", "spring", "spring", "summer", "summer", "summer", "autumn", "autumn", "autumn", "winter"];
+  
+  return monthBySeason[date.getMonth()];
 }
 
 module.exports = {
